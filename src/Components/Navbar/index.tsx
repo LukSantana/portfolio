46089990a2227909c60d7e024820c85d59638822
Luk Santana <lucas.diassantana@gmail.com>
2022-10-05 22:52:06 -0300

    Improved styles

--- src/Components/Navbar/index.tsx
+++ src/Components/Navbar/index.tsx
@@ -22,7 +22,7 @@ export default function Header() {
                         </a>
                     </div>
                     <div className="-my-2 -mr-2 md:hidden">
-                        <Popover.Button className="inline-flex items-center justify-center rounded-md bg-[#212529] p-2 text-gray-400 hover:bg-[#212529] focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                        <Popover.Button className="inline-flex items-center justify-center rounded-md bg-[#212529] p-2 text-gray-400 hover:bg-[#212529] focus:outline-none focus:ring-2 focus:ring-inset focus:ring-stone-500">
                             <span className="sr-only">Open menu</span>
                             <Bars3Icon className="h-6 w-6" aria-hidden="true" />
                         </Popover.Button>
@@ -73,7 +73,7 @@ export default function Header() {
                                     />
                                 </div>
                                 <div className="-mr-2">
-                                    <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                                    <Popover.Button className="inline-flex items-center justify-center rounded-md bg-[#212529] p-2 text-gray-400 hover:bg-[#32383e] hover:text-gray-300 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-stone-400">
                                         <span className="sr-only">Close menu</span>
                                         <XMarkIcon className="h-6 w-6" aria-hidden="true" />
                                     </Popover.Button>
--- src/Components/SkillsCard/index.tsx
+++ src/Components/SkillsCard/index.tsx
@@ -7,8 +7,8 @@ interface SkillCardProps {
 const SkillsCard = ({ title, imgUrl, skillLevel }: SkillCardProps) => {
     return (
         <div className='flex flex-col justify-center items-center gap-3 w-24'>
-            <h2 className='text-center text-white'>{title}</h2>
             <img src={imgUrl} alt={title} />
+            <h2 className='text-center text-white'>{title}</h2>
             <div className='bg-[#4a5158] rounded-lg h-1 w-full'>
                 <div className={`bg-gradient-to-r from-purple-500 via-purple-600 to-purple-800 rounded-lg h-1`} style={{width: `${skillLevel}`}}></div>
             </div>
