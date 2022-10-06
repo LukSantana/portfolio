7b44b504b21fa431c3376b335753f207c11b84ea
Luk Santana <lucas.diassantana@gmail.com>
2022-10-06 00:05:37 -0300

    Added target to anchour tags

--- src/Components/Contact/index.tsx
+++ src/Components/Contact/index.tsx
@@ -8,9 +8,9 @@ const Contact = () => {
         <Heading>Contato</Heading>
         <h2 className='text-white text-2xl'>Entre em contato comigo:</h2>
         <div className="grid grid-cols-1 sm:grid-cols-3 gap-8 justify-items-center text-white">
-            <a href="https://github.com/LukSantana" className="flex text-xl gap-2 transform ease-in-out hover:scale-105"><BsGithub size="1.5rem"/>LukSantana</a>
-            <a href="https://www.linkedin.com/in/luksantana/" className="flex text-xl gap-2 transform ease-in-out hover:scale-105"><BsLinkedin size="1.5rem"/>LukSantana</a>
-            <a href="mailto:lucas.diassantana@gmail.com" className="flex text-xl gap-2 transform ease-in-out hover:scale-105"><FaEnvelope size="1.5rem"/>LukSantana</a>
+            <a href="https://github.com/LukSantana" target="_blank" className="flex text-xl gap-2 transform ease-in-out hover:scale-105"><BsGithub size="1.5rem"/>LukSantana</a>
+            <a href="https://www.linkedin.com/in/luksantana/" target="_blank" className="flex text-xl gap-2 transform ease-in-out hover:scale-105"><BsLinkedin size="1.5rem"/>LukSantana</a>
+            <a href="mailto:lucas.diassantana@gmail.com" target="_blank" className="flex text-xl gap-2 transform ease-in-out hover:scale-105"><FaEnvelope size="1.5rem"/>LukSantana</a>
         </div>
     </div>
   )
--- src/Components/ProjectCard/index.tsx
+++ src/Components/ProjectCard/index.tsx
@@ -9,13 +9,13 @@ interface ProjectCardProps {
 const ProjectCard = ({ imgUrl, title, description, projectUrl, gitHubUrl }: ProjectCardProps) => {
     return (
         <div className='flex flex-col min-h-full justify-start items-center gap-4 bg-[#212529] text-white relative m-6 rounded-lg'>
-            <a className="cursor-pointer" href={projectUrl}>
+            <a className="cursor-pointer" href={projectUrl} target="_blank">
                 <img className='w-full object-contain rounded-t-lg brightness-75' src={imgUrl} alt="project img" />
             </a >
             <div className="flex flex-col items-center justify-center gap-3 px-3">
                 <h2 className='font-medium text-2xl'>{title}</h2>
                 <span className='break-words text-center px-2'>{description}</span>
-                <a className='border-2 rounded-lg px-3 py-1 mb-4 cursor-pointer transition ease-in-out hover:scale-105' href={gitHubUrl}>Github</a>
+                <a className='border-2 rounded-lg px-3 py-1 mb-4 cursor-pointer transition ease-in-out hover:scale-105' href={gitHubUrl} target="_blank">Github</a>
             </div>
         </div>
     )
