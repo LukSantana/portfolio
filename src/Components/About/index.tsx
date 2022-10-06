8addcf3a4ef45e50c3fe2ab4ec8be2aa4c113789
Lucas Santana <lucas.diassantana@gmail.com>
2022-10-05 22:52:06 -0300

    Improved styles

--- src/Components/About/index.tsx
+++ src/Components/About/index.tsx
@@ -6,9 +6,9 @@ const About = () => {
     return (
         <div className='flex justify-center sm:flex-row flex-col items-center gap-24 h-fit py-12'>
             <div className="flex justify-end sm:w-[50%]"><img className='h-80' src="../../assets/img/developer_cartoon.png" alt="Delevoper Cartoon Image" /></div>
-            <div className="flex flex-col gap-4 justify-center sm:w-[50%] px-4">
+            <div className="flex flex-col gap-4 justify-center sm:w-[50%] sm:px-0 px-6">
                 <Heading>Sobre Mim</Heading>
-                <span className="break-words text-white pr-8 text-lg">
+                <span className="break-words text-white text-lg  sm:pr-[40%]">
                     Meu nome é Lucas Santana, sou um desenvolvedor fullstack estudando
                     de forma autodidata desde o começo de 2022 e estou sempre disposto
                     a conhecer e aprender novas coisas.
