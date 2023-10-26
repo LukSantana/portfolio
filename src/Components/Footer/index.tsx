e8a2e3417551aeccf31d5c003caf82fe9917acd0
Lucas Santana <lucas.diassantana@gmail.com>
2023-10-26 19:30:43 -0300

    updated footer

--- src/Components/Footer/index.tsx
+++ src/Components/Footer/index.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='flex items-center justify-center bg-gray-500 bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-30 w-full h-[4rem] text-white'>
-        <span>© Lucas Santana 2022</span>
+        <span>© Lucas Santana 2023</span>
     </div>
   )
 }
