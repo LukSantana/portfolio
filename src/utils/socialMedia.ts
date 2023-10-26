901b4bef1276f5658d67a5437259ab53347e1c8e
Luk Santana <lucas.diassantana@gmail.com>
2023-10-26 18:22:46 -0300

    updated social media

--- src/utils/socialMedia.ts
+++ src/utils/socialMedia.ts
@@ -1,21 +1,30 @@
 import { IconType } from "react-icons";
-import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { FaEnvelope, FaGithub, FaLinkedin } from "react-icons/fa";
 
 interface SocialMediaProps {
   title: string,
   icon: IconType,
   url: string
+  username: string
 }
 
 export const socialMedia: Array<SocialMediaProps> = [
   {
     title: "GitHub",
     icon: FaGithub,
-    url: "https://github.com/LukSantana"
+    url: "https://github.com/LukSantana",
+    username: "LukSantana"
   },
   {
     title: "Linkedin",
     icon: FaLinkedin,
-    url: "https://www.linkedin.com/in/luksantana"
+    url: "https://www.linkedin.com/in/luksantana",
+    username: "LukSantana"
+  },
+  {
+    title: "Email",
+    icon: FaEnvelope,
+    url: "@mail:lucas.diassantana@gmail.com",
+    username: "Email"
   }
 ]
